058b814204c7f777880cc4270855e232d81841cb
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-08-28 12:09:48 -0400

    WIP

--- spec/people/requests/create_birthdate_spec.rb
+++ spec/people/requests/create_birthdate_spec.rb
@@ -3,8 +3,26 @@ require 'rails_helper'
 RSpec.describe 'create_birthdate mutation' type: :request do
   let(:endpoint) { '/graphql' }
   let(:person) { create(:person) }
-  let(:birth_year) { 2005 }
-  let(:birth_month) { 'November' }
-  let(:birth_day) { 27 }
+  let(:birth_year) { 1937 }
+  let(:birth_month) { 'May' }
+  let(:birth_day) { 8 }
+  let(:query_string) do
+    "
+        mutation CreateBirthdate($input: CreateBirthdateInput!) {
+            createBirthdate(input: $input) {
+                person {
+                    id
+                    birthYear
+                    birthMonth
+                    birthDay
+                }
+                errors {
+                    path
+                    message
+                }
+            }
+        }
+    "
+  end
 
 end
