d6c1fcf8023e6cd516d5e911e3b7133aed24e9a1
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-11-09 10:41:08 -0500

    Adjust Lograge and general Rails logging config

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -6,9 +6,26 @@ Rails.application.configure do
     config.lograge.formatter = Lograge::Formatters::Json.new
     config.colorize_logging = false
 
+    config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
+
     config.lograge.custom_options = lambda do |event|
-      { :params => event.payload[:params],
-        :level => event.payload[:level]
+      # Retrieves trace information for current thread
+      correlation = Datadog.tracer.active_correlation
+  
+      {
+        # Adds IDs as tags to log output
+        :dd => {
+          # To preserve precision during JSON serialization, use strings for large numbers
+          :trace_id => correlation.trace_id.to_s,
+          :span_id => correlation.span_id.to_s,
+          :env => correlation.env.to_s,
+          :service => correlation.service.to_s,
+          :version => correlation.version.to_s
+        },
+        :ddsource => ["ruby"],
+        # :params => event.payload[:params].reject { |k| %w(controller action).include? k },
+        :params => event.payload[:params],
+        :level => event.payload[:level]        
       }
     end
 end
