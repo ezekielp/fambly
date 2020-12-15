cceb474abfc2d5e7fbd2af72b08f8b94e290d2de
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-12-15 16:09:17 -0500

    WIP

--- app/javascript/client/profiles/emails/EmailForm.tsx
+++ app/javascript/client/profiles/emails/EmailForm.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
+import { useCreateEmailMutation } from 'client/graphqlTypes';
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { EMAIL_TYPE_OPTIONS } from './utils';
 import { Button } from 'client/common/Button';
 import { Text } from 'client/common/Text';
+import { SectionDivider } from 'client/profiles/PersonContainer';
 import * as yup from 'yup';
 import { gql } from '@apollo/client';
 import { handleFormErrors } from 'client/utils/formik';
@@ -22,3 +24,84 @@ gql`
     }
   }
 `;
+
+export const EmailFormValidationSchema = yup.object().shape({
+  email: yup.string().required().email().label('Email'),
+  emailType: yup.string(),
+});
+
+export interface EmailFormData {
+  email: string;
+  emailType?: string;
+}
+
+export interface EmailFormProps {
+  setFieldToAdd?: (field: string) => void;
+  personId: string;
+  initialValues?: EmailFormData;
+  setEditFlag?: (bool: boolean) => void;
+  setModalOpen?: (bool: boolean) => void;
+}
+
+export const blankInitialValues: EmailFormData = {
+  email: '',
+  emailType: '',
+};
+
+export const EmailForm: FC<EmailFormProps> = ({
+  setFieldToAdd,
+  personId,
+  initialValues = blankInitialValues,
+  setEditFlag,
+  setModalOpen,
+}) => {
+  const [createEmailMutation] = useCreateEmailMutation();
+
+  const cancel = () => {
+    if (setFieldToAdd) {
+      setFieldToAdd('');
+    } else if (setEditFlag && setModalOpen) {
+      setEditFlag(false);
+      setModalOpen(false);
+    }
+  };
+
+  const handleSubmit = async (
+    data: EmailFormData,
+    formikHelpers: FormikHelpers<EmailFormData>,
+  ) => {
+    const { email, emailType } = data;
+    if (!email) return;
+    const { setErrors, setStatus } = formikHelpers;
+
+    const response = await createEmailMutation({
+      variables: {
+        input: {
+          personId,
+          email,
+          emailType: emailType ? emailType : null,
+        },
+      },
+    });
+    const errors = response.data?.createEmail.errors;
+
+    if (errors) {
+      handleFormErrors<EmailFormData>(errors, setErrors, setStatus);
+    } else {
+      if (setFieldToAdd) {
+        setFieldToAdd('');
+      } else if (setEditFlag) {
+        setEditFlag(false);
+        setModalOpen && setModalOpen(false);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Text marginBottom={3} fontSize={4} bold>
+        Email
+      </Text>
+    </>
+  );
+};
