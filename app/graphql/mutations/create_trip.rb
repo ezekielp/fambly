4cab9d1fd067f73a7a3d34b439b0886e69d14ecc
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-12-23 19:42:31 -0500

    Add create_trip mutation with spec

--- app/graphql/mutations/create_trip.rb
+++ app/graphql/mutations/create_trip.rb
@@ -1,9 +1,77 @@
 module Types
   class CreateTripInputType < Types::BaseInputObject
-
+    argument :departure_country, String, required: false
+    argument :departure_town, String, required: false
+    argument :departure_region, String, required: false
+    argument :end_country, String, required: false
+    argument :end_town, String, required: false
+    argument :end_region, String, required: false
+    argument :departure_day, Int, required: false
+    argument :departure_month, Int, required: false
+    argument :departure_year, Int, required: false
+    argument :end_day, Int, required: false
+    argument :end_month, Int, required: false
+    argument :end_year, Int, required: false
+    argument :note, String, required: false
   end
 end
 
-# Need all the (optional) info for both departure and end points
-# Also need all the (optional) departure and end date info
-# I'm thinking you make adding people to the trip separate, both in the UI and in this mutation
+module Mutations
+  class CreateTrip < BaseMutation
+    argument :input, Types::CreateTripInputType, required: true
+
+    field :trip, Types::TripType, null: true
+
+    def resolve(input:)
+      departure_country, departure_town, departure_region = input.departure_country, input.departure_town, input.departure_region
+
+      end_country, end_town, end_region = input.end_country, input.end_town, input.end_region
+
+      departure_point, end_point = nil, nil
+
+      if departure_country || departure_town || departure_region
+        departure_point = Place.find_or_create_by(
+          country: departure_country,
+          state_or_region: departure_region,
+          town: departure_town
+        )
+      end
+
+      if end_country || end_town || end_region
+        end_point = Place.find_or_create_by(
+          country: end_country,
+          state_or_region: end_region,
+          town: end_town
+        )
+      end
+
+      new_trip = Trip.new(
+        user_id: current_user.id,
+        departure_point_id: departure_point ? departure_point.id : nil,
+        end_point_id: end_point ? end_point.id : nil,
+        departure_day: input.departure_day,
+        departure_month: input.departure_month,
+        departure_year: input.departure_year,
+        end_day: input.end_day,
+        end_month: input.end_month,
+        end_year: input.end_year
+      )
+
+      if new_trip.save
+        if input.note
+          trip_note = Note.new(
+            content: input.note,
+            notable: new_trip
+          )
+          saved = trip_note.save
+
+          return { errors: [{ path: '', message: 'Oops! Something went wrong. Please refresh the page and try again.' }] } unless saved
+        end
+
+        return { trip: new_trip }
+      end
+
+      { errors: [{ path: '', message: 'Oops! Something went wrong. Please refresh the page and try again.' }] }
+    end
+  end
+end
--- spec/trips/requests/create_trip_spec.rb
+++ spec/trips/requests/create_trip_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe 'create_trip mutation', type: :request do
   let(:endpoint) { '/graphql' }
   let(:user) { create(:user) }
-  let(:departure_point) { Place.create(country: 'England', town: 'Plymouth', name: 'Plymouth Sound') }
-  let(:end_point) { Place.create(country: 'England') }
+  let(:password) { 'Schwarzgerat' }
+  let(:country) { 'England' }
+  let(:town) { 'Plymouth' }
   let(:departure_day) { 27 }
   let(:departure_month) { 12 }
   let(:departure_year) { 1831 }
   let(:end_month) { 9 }
   let(:end_year) { 1835 }
+  let(:note) { 'Considering the small size of these islands, we feel the more astonished at the number of their aboriginal beings, and at their confined range... within a period geologically recent the unbroken ocean was here spread out. Hence, both in space and time, we seem to be brought somewhat near to that great fact – that mystery of mysteries – the first appearance of new beings on this earth.' }
   let(:query_string) do
     "
         mutation CreateTrip($input: CreateTripInput!) {
@@ -52,26 +54,30 @@ RSpec.describe 'create_trip mutation', type: :request do
   let(:variables) do
     {
         input: {
-          departure_point_id: departure_point.id,
-          end_point_id: end_point.id,
-          departure_day: departure_day,
-          departure_month: departure_month,
-          departure_year: departure_year,
-          end_month: end_month,
-          end_year: end_year,
+          departureCountry: country,
+          departureTown: town,
+          endCountry: country,
+          departureDay: departure_day,
+          departureMonth: departure_month,
+          departureYear: departure_year,
+          endMonth: end_month,
+          endYear: end_year,
+          note: note
         }
     }
   end
 
   it 'creates a new trip for an associated user' do
+    login_user(email: user[:email], password: password)
+
     post(
       endpoint,
       params: { query: query_string, variables: variables.to_json }
     )
 
     trip = JSON.parse(response.body).dig('data', 'createTrip', 'trip')
-    expect(trip['departurePoint']).to eq(departure_point)
-    expect(trip['endPoint']).to eq(end_point)
+    expect(trip['departurePoint']['town']).to eq(town)
+    expect(trip['endPoint']['country']).to eq(country)
     expect(user.trips.first.departure_year).to eq(departure_year)
     expect(user.trips.first.end_year).to eq(end_year)
   end
